ecadc87fe9e17fa2d776853dac3cfe93e30d2f61
4JN22CS147 <gowdasharmadar@gmail.com>
2026-02-06 15:39:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    document.getElementById("msg").innerHTML =
+        "JavaScript is working in index.html!";
+}
+
+function showAlert() {
+    alert("JavaScript is working in javascript.html!");
+}
